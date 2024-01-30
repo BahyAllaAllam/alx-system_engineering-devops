73e04745ed9ff30ff43d690e5412d8acbbe2b7ed
BahyAllaAllam <bahy.alam@yahoo.com>
2024-01-30 14:55:01 -0800

    2-repetition_token_1.rb

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+def match_pattern(text)
+  regex = /h(t|bt)n/
+  match = text.match(regex)
+
+  if match
+    puts match.to_s
+  end
+end
+
+if ARGV.length == 1
+  text_to_search = ARGV[0]
+  match_pattern(text_to_search)
+end
