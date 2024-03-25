5bb48b6a4d17533be5af08f4e59812a835febd30
BahyAllaAllam <bahy.alam@yahoo.com>
2024-03-25 03:49:13 -0700

    fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,16 @@
-# Set up client SSH configuration file so that to connect to a server without typing a password.
+# Set up client SSH configuration file so that to connect to a server without typing a password
 
 file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/ssh_config',
-  line  => 'PasswordAuthentication no',
+  path    => '/etc/ssh/ssh_config',
+  line    => '^PasswordAuthentication\s+yes$',
+  match   => '^PasswordAuthentication\s+yes$',
+  replace => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
-  path  => '/etc/ssh/ssh_config',
-  line  => 'IdentityFile ~/.ssh/school',
+  path    => '/etc/ssh/ssh_config',
+  line    => '^#?\s*IdentityFile\s+~/.ssh/school$',
+  match   => '^#?\s*IdentityFile\s+~/.ssh/school$',
+  replace => 'IdentityFile ~/.ssh/school',
 }
 
